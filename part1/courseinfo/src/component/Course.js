b7069fcb142401e7fdc4f62bbe84fd13245560f9
naiiiden <na4ko359@gmail.com>
2023-04-08 16:12:01 +0300

    add h1, main, header tags + step2 2.5

--- part1/courseinfo/src/component/Course.js
+++ part1/courseinfo/src/component/Course.js
@@ -4,14 +4,19 @@ import Header from "./Header";
 const Course = ({ courses }) => {
     return (
         <div>
-            {courses.map(course => {
-                return (
-                    <div key={course.id}>
-                        <Header course={course.name}/>
-                        <Content parts={course.parts}/>
-                    </div>
-                )
-            })}
+            <header>
+                <h1>Web development curriculum</h1>
+            </header>
+            <main>
+                {courses.map(course => {
+                    return (
+                        <div key={course.id}>
+                            <Header course={course.name}/>
+                            <Content parts={course.parts}/>
+                        </div>
+                    )
+                })}
+            </main>
         </div>
     )
 }
