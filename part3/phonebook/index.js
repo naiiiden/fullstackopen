415608fb54bbebbdced6e41921f949c2e5e42f5c
naiiiden <na4ko359@gmail.com>
2023-04-12 10:40:40 +0300

    part3c phonebook install dotenv

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 app.use(express.json());
+require('dotenv').config();
 
 const mongoose = require('mongoose')
 
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =
-  `mongodb+srv://fullstack:fso@cluster0.6gnwje6.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -26,67 +25,13 @@ morgan.token('body', req => {
 
 app.use(morgan(':method :url :body'));
 
-// let persons = [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": 4,
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ];
-
 app.get('/api/persons', (req, res) => {
   Phone.find({}).then(persons => {
     res.json(persons)
   })
 });
 
-const generateId = () => {
-  const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-  return maxId + 1;
-}
-
 app.post('/api/persons', (req, res) => {
-  // const body = req.body;
-
-  // if (!body.name || !body.number) {
-  //   return res.status(404).json({
-  //     error: 'name or number is missing'
-  //   });
-  // }
-
-  // if(persons.find((person) => person.number === body.number)) {
-  //   return res.status(404).json({
-  //     error: 'that number already exists in phonebook'
-  //   });
-  // }
-
-
-  // const person = {
-  //   "id": generateId(),
-  //   "name": body.name,
-  //   "number": body.number,
-  // }
-
-  // persons = persons.concat(person);
-  // res.json(person);
-
   const body = req.body;
     
     if (body.number === undefined) {
@@ -123,6 +68,6 @@ app.get('/info', (req, res) => {
   res.send(`<div>Phonebook has info for ${persons.length} people <br/>${new Date()}</div>`);
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT, () => {
     console.log(`server running on port 3001`);
 });
