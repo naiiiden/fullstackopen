66974ae6b999d8cd342a5e9aa48e8a3dae9fd660
naiiiden <na4ko359@gmail.com>
2023-04-13 20:34:32 +0300

    part4a exercise 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,46 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:fso@cluster0.6gnwje6.mongodb.net/blogListApp?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+    logger.info(`server running on ${config.PORT}`)
 })
