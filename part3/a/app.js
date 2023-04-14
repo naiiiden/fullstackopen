023b2d3e8728c6018eaf550550d9071c62305a4f
naiiiden <na4ko359@gmail.com>
2023-04-14 18:25:19 +0300

    part4 user/token auth

--- part3/a/app.js
+++ part3/a/app.js
@@ -2,11 +2,15 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const notesRouter = require('./controllers/notes')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const usersRouter = require('./controllers/user')
+require('express-async-errors')
+
+const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -27,6 +31,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
