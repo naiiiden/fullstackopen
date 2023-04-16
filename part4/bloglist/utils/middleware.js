abe949957a6156e554a557016bd3356bbcd07f52
naiiiden <na4ko359@gmail.com>
2023-04-16 15:50:55 +0300

    part4 bloglist revoke token in 60min

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -21,6 +21,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return res.status(400).json({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: 'token expired '})
   }
 
   next(error)
