0b338013fc1524d404739fc5db3d9af39678c863
naiiiden <na4ko359@gmail.com>
2023-04-20 23:26:26 +0300

    part 6d exercise 6.22

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,17 +1,32 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from 'react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
-  const result = useQuery('anecdotes', getAnecdotes)
-
+  const queryClient = useQueryClient()
+  
+  const VoteAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
+  
+  const handleAnecdote = (anecdote) => {
+    VoteAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
+  }
+  
+  
+  const result = useQuery('anecdotes', getAnecdotes, {
+    refetchOnWindowFocus: false
+  })
+  
   if ( result.isLoading ) {
     return <div>loading data...</div>
   }
   
   const anecdotes = result.data
-
+  
   return (
     <div>
       <h3>Anecdote app</h3>
@@ -24,7 +39,7 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => console.log(anecdote.votes)}>vote</button>
+            <button onClick={() => handleAnecdote(anecdote)}>vote</button>
           </div>
         </div>
       )}
