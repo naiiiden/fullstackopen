63b795f9e72c5ec077627af7606636f2c2eb8b22
naiiiden <na4ko359@gmail.com>
2023-04-20 19:33:04 +0300

    move addAnecdote function to AnecdoteForm.js component

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,27 +1,10 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery, useMutation, useQueryClient } from 'react-query'
-import { createAnecdote, getAnecdotes } from './requests'
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
 
 const App = () => {
-  const queryClient = useQueryClient()
-
   const result = useQuery('anecdotes', getAnecdotes)
-  
-  const newAnecdoteMutation = useMutation(createAnecdote, {
-    onSuccess: (newAnecdote) => {
-      const anecdotes = queryClient.getQueryData('anecdotes')
-      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
-    }
-  })
-
-  const addAnecdote = async (e) => {
-    e.preventDefault()
-    const content = e.target.anecdote.value
-    console.log(content)
-    e.target.anecdote.value = ''
-    newAnecdoteMutation.mutate({ content, votes: 0 })
-  }
 
   if ( result.isLoading ) {
     return <div>loading data...</div>
@@ -32,10 +15,6 @@ const App = () => {
   return (
     <div>
       <h3>Anecdote app</h3>
-      <form onSubmit={addAnecdote}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
       <Notification />
       <AnecdoteForm />
       {anecdotes?.map(anecdote =>
--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,16 +1,28 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from 'react-query'
+import { createAnecdote } from '../requests'
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    }
+  })
+
+  const addAnecdote = async (e) => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    console.log(content)
+    e.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
 
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <form onSubmit={addAnecdote}>
         <input name='anecdote' />
         <button type="submit">create</button>
       </form>
