a1569fab171337e8e16e68e8254253389f625034
naiiiden <na4ko359@gmail.com>
2023-04-23 17:35:40 +0300

    part 8a 8.2: All books

--- part8/library-graphql/index.js
+++ part8/library-graphql/index.js
@@ -27,12 +27,6 @@ let authors = [
   },
 ]
 
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
 let books = [
   {
     title: 'Clean Code',
@@ -85,21 +79,26 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
-
 const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
