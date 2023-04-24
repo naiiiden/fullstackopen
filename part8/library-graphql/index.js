b85b51c025238eafc1a2bf113cb854a10ac21efa
naiiiden <na4ko359@gmail.com>
2023-04-24 10:48:43 +0300

    part 8 graphql return all authors(no bookcount 4 now)

--- part8/library-graphql/index.js
+++ part8/library-graphql/index.js
@@ -80,17 +80,26 @@ let books = [
 ]
 
 const typeDefs = `
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+
   type Book {
     title: String!
     author: String!
     published: Int!
     genres: [String!]!
+    id: ID!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -99,6 +108,7 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors
   }
 }
 
