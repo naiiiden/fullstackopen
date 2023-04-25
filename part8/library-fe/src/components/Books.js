4fb88ae3375a28271456b78960444bbd62cba398
naiiiden <na4ko359@gmail.com>
2023-04-25 16:59:58 +0300

    part 8b add queries.js & update cache on book creation

--- part8/library-fe/src/components/Books.js
+++ part8/library-fe/src/components/Books.js
@@ -1,15 +1,5 @@
-import { gql, useQuery } from '@apollo/client'
-
-const ALL_BOOKS = gql`
-  query {
-    allBooks {
-      title
-      author
-      published
-      id
-    }
-  }
-`
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
   const books = useQuery(ALL_BOOKS)
