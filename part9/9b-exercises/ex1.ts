b9a18379c5c48bbde9d2dbdfe233cfc3e00fffa5
naiiiden <na4ko359@gmail.com>
2023-04-26 14:45:19 +0300

    part 9b exercise 3 cli

--- part9/9b-exercises/ex1.ts
+++ part9/9b-exercises/ex1.ts
@@ -1,15 +1,18 @@
 const bmiCalculator = (weight: number, height: number) => {
     const result: number = weight / (height * height)
     switch (true) {
-        case result < 16: return 'Underweight (severe thinness)' 
-        case result < 16.9: return 'Underweight (Moderate thinness)'
-        case result < 18.4: return 'Underweight (Mild thinness)'
-        case result < 24.9: return 'Normal (healthy weight)'
-        case result < 29.9: return 'Overweight (Pre-obese)'
-        case result < 34.9: return 'Obese (Class I)'
-        case result < 39.9: return 'Obese (Class II)'
-        default: return 'Obese (Class III)'
+        case result < 16: console.log('Underweight (severe thinness)')  
+        case result < 16.9: console.log('Underweight (Moderate thinness)') 
+        case result < 18.4: console.log('Underweight (Mild thinness)') 
+        case result < 24.9: console.log('Normal (healthy weight)') 
+        case result < 29.9: console.log('Overweight (Pre-obese)') 
+        case result < 34.9: console.log('Obese (Class I)') 
+        case result < 39.9: console.log('Obese (Class II)') 
+        default: console.log('Obese (Class III)') 
     }
 }
 
-console.log(bmiCalculator(60, 1.75)) 
+const weight = Number(process.argv[2])
+const height = Number(process.argv[3])
+
+bmiCalculator(weight, height)
