b246156c75c628768a758a2f8510816d09970e80
naiiiden <na4ko359@gmail.com>
2023-04-26 16:08:38 +0300

    /bmi

--- part9/9b-exercises/index.ts
+++ part9/9b-exercises/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import { bmiCalculator } from './ex1';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (_req, res) => {
+    res.send(bmiCalculator(60, 1.75))
 });
 
 const PORT = 3003;
